0ca9842c0c51d93f376a9824f718e301d3826641
Jason Efstathiou <jason.efst@gmail.com>
2018-10-24 11:02:27 +0200

    fix url matching

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'getgrover.com'},
+            pageUrl: {urlContains: 'getgrover.com'},
           })
           ],
               actions: [new chrome.declarativeContent.ShowPageAction()]
